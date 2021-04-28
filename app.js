93bd414add419645c37ce05fc13ab636d9429ba8
Cintia Garcia <cintiagarcia@air-de-cintia.lan>
2021-04-28 18:11:27 +0200

    imageWalletfix

--- app.js
+++ app.js
@@ -20,6 +20,29 @@ hbs.registerHelper("times", function (n, block) {
   return accum;
 });
 
+hbs.registerHelper("ratings", function (n, block) {
+  var accum = "";
+  for (var i = 0; i < 5; ++i) {
+    if (n > i) {
+      accum += '<span class="fa fa-star checked"></span>';
+    } else {
+      accum += '<span class="fa fa-star unchecked"></span>';
+    }
+  }
+  return accum;
+});
+
+hbs.registerHelper("image", function (imagePath, block) {
+
+    if (imagePath == "") {
+      return `<img src="iconPlaceholder.png" width="100px" heigth="100px" alt="image missing">`;
+    } else {
+      return  `<img src=${imagePath} width="100px" heigth="100px" alt="image missing">`;
+    }
+
+});
+
+
 const app = express();
 
 // ℹ️ This function is getting exported from the config folder. It runs most middlewares
@@ -27,11 +50,10 @@ require("./config")(app);
 
 // session configuration
 
-const session = require('express-session');
-const MongoStore = require('connect-mongo');
-const mongoose = require('./db/index');
-const DB_URL = 'mongodb://localhost/passport';
-
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
+const mongoose = require("./db/index");
+const DB_URL = "mongodb://localhost/passport";
 
 app.use(
   session({
@@ -40,61 +62,63 @@ app.use(
     saveUninitialized: false,
     resave: true,
     store: MongoStore.create({
-      mongoUrl: DB_URL
-    })
+      mongoUrl: DB_URL,
+    }),
   })
-)
+);
+
+app.use(express.static("public/images")); 
+
 
 // end of session configuration
 
-
 // passport configuration
 // http://www.passportjs.org/docs/configure/
-const { User, Wallet } = require('./models/models');
-const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
-const bcrypt = require('bcrypt');
+const { User, Wallet } = require("./models/models");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const bcrypt = require("bcrypt");
 
-// passport wants to store as little data as possible in the session so it only uses 
-// the id's (or someting else if we would want to implement that) and not the whole 
+// passport wants to store as little data as possible in the session so it only uses
+// the id's (or someting else if we would want to implement that) and not the whole
 // user object
 // this method is used by passport to put the id of the user into the session
 passport.serializeUser((user, done) => {
   done(null, user._id);
-})
+});
 
 // this is used to retrieve the user by it's id (that is stored in the session)
 passport.deserializeUser((id, done) => {
   User.findById(id)
-    .then(dbUser => {
-      done(null, dbUser)
+    .then((dbUser) => {
+      done(null, dbUser);
     })
-    .catch(err => {
+    .catch((err) => {
       done(err);
-    })
-})
+    });
+});
 
 passport.use(
   new LocalStrategy((username, password, done) => {
     // this logic will be executed when we log in
     User.findOne({ username: username })
-      .then(userFromDB => {
+      .then((userFromDB) => {
         if (userFromDB === null) {
           // there is no user with this username
-          done(null, false, { message: 'Wrong Credentials' });
+          done(null, false, { message: "Wrong Credentials" });
         } else if (!bcrypt.compareSync(password, userFromDB.password)) {
           // the password does not match
-          done(null, false, { message: 'Wrong Credentials' });
+          done(null, false, { message: "Wrong Credentials" });
         } else {
           // everything correct - user should be logged in
           done(null, userFromDB);
         }
       })
-      .catch(err => {
+      .catch((err) => {
         next(err);
-      })
+      });
   })
-)
+);
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -121,14 +145,12 @@ app.use("/", login);
 const reviews = require("./routes/reviews");
 app.use("/", reviews);
 
-
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
 
 module.exports = app;
 
-
-// validation email 
+// validation email
 
 function validateEmail(email) {
   const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
