192e50037bd1f1c89a79c5777c141113546406d1
Fintan Hahnefeld <fintan22@gmail.com>
2021-04-29 19:57:34 +0200

    test3

--- app.js
+++ app.js
@@ -106,7 +106,7 @@ app.use(
     saveUninitialized: false,
     resave: true,
     store: MongoStore.create({
-      mongoUrl: MONGODB_URI //DB_URL ,
+      mongoUrl: 'mongodb+srv://Ironhack:UvBXyfMF56DwFEjD@cluster0.0e5ts.mongodb.net/Crypto-Wallet-Reviews?retryWrites=true&w=majority' //DB_URL ,
     }),
   })
 );
@@ -115,58 +115,6 @@ app.use(express.static("public/images"));
 
 // end of session configuration
 
-// passport configuration
-// http://www.passportjs.org/docs/configure/
-const { User, Wallet } = require("./models/models");
-const passport = require("passport");
-const LocalStrategy = require("passport-local").Strategy;
-const bcrypt = require("bcrypt");
-
-// passport wants to store as little data as possible in the session so it only uses
-// the id's (or someting else if we would want to implement that) and not the whole
-// user object
-// this method is used by passport to put the id of the user into the session
-passport.serializeUser((user, done) => {
-  done(null, user._id);
-});
-
-// this is used to retrieve the user by it's id (that is stored in the session)
-passport.deserializeUser((id, done) => {
-  User.findById(id)
-    .then((dbUser) => {
-      done(null, dbUser);
-    })
-    .catch((err) => {
-      done(err);
-    });
-});
-
-passport.use(
-  new LocalStrategy((username, password, done) => {
-    // this logic will be executed when we log in
-    User.findOne({ username: username })
-      .then((userFromDB) => {
-        if (userFromDB === null) {
-          // there is no user with this username
-          done(null, false, { message: "Wrong Credentials" });
-        } else if (!bcrypt.compareSync(password, userFromDB.password)) {
-          // the password does not match
-          done(null, false, { message: "Wrong Credentials" });
-        } else {
-          // everything correct - user should be logged in
-          done(null, userFromDB);
-        }
-      })
-      .catch((err) => {
-        next(err);
-      });
-  })
-);
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-// end of passport configurations
 
 // default value for title local
 const projectName = "Crypto-Wallet-Reviews";
